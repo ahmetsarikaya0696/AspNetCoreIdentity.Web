a136685a7f40e62e95e482954fb515c765eeeef5
ahmetsarikaya0696 <ahmetsarikaya0696@gmail.com>
2024-03-23 13:38:22 +0300

    IdentiyDbContext

---
+++ WebApplication1/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class AppDbContext : IdentityDbContext<AppUser, AppRole, string>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
