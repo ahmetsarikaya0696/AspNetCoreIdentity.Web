ee07edeb7a8fa344b79482a416076546c4702dc6
ahmetsarikaya0696 <ahmetsarikaya0696@gmail.com>
2024-03-23 20:42:24 +0300

    Admin area içinde Userlist sayfasının oluşturulması

---
+++ WebApplication1/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using AspNetCoreIdentity.Web.Areas.Admin.Models;
+using AspNetCoreIdentity.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreIdentity.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> UserList()
+        {
+            List<UserViewModel> users = await _userManager.Users.Select(x => new UserViewModel()
+            {
+                Id = x.Id,
+                Email = x.Email,
+                UserName = x.UserName
+            }).ToListAsync();
+
+            return View(users);
+        }
+    }
+}
