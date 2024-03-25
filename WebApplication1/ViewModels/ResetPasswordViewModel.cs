4028b4fa35cd32cd229b56dacfea2d2dbc8fb4fe
ahmetsarikaya0696 <ahmetsarikaya0696@gmail.com>
2024-03-25 19:54:33 +0300

    Şifre sıfırlama sayfasının oluşturulması

---
+++ WebApplication1/ViewModels/ResetPasswordViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentity.Web.ViewModels
+{
+    public class ResetPasswordViewModel
+    {
+        [EmailAddress(ErrorMessage = "E-posta formatı yanlıştır")]
+        [Required(ErrorMessage = "E-posta alanı boş bırakılamaz")]
+        [Display(Name = "E-posta")]
+        public string Email { get; set; }
+    }
+}
