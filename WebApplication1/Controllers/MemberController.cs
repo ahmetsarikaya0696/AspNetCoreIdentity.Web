de32ca395356389661d99f4029d47be10f9e5121
ahmetsarikaya0696 <ahmetsarikaya0696@gmail.com>
2024-03-27 00:41:53 +0300

    Kullanıcı profil sayfasının tasarlanması

--- WebApplication1/Controllers/MemberController.cs
+++ WebApplication1/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
 ﻿using AspNetCoreIdentity.Web.Models;
+using AspNetCoreIdentity.Web.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -9,10 +10,12 @@ namespace AspNetCoreIdentity.Web.Controllers
     public class MemberController : Controller
     {
         private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
 
-        public MemberController(SignInManager<AppUser> signInManager)
+        public MemberController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
         }
 
         public async Task Logout()
@@ -20,9 +23,11 @@ namespace AspNetCoreIdentity.Web.Controllers
             await _signInManager.SignOutAsync();
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var currentUser = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserInfoViewModel userInfoViewModel = new() { Id = currentUser.Id, UserName = currentUser.UserName, Email = currentUser.Email };
+            return View(userInfoViewModel);
         }
     }
 }
---
+++ WebApplication1/ViewModels/UserInfoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetCoreIdentity.Web.ViewModels
+{
+    public class UserInfoViewModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
