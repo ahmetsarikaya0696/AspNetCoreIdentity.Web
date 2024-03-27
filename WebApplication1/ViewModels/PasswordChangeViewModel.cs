542a4f299ca834029b4482763d279c5f59133f8d
ahmetsarikaya0696 <ahmetsarikaya0696@gmail.com>
2024-03-27 23:17:26 +0300

    PasswordChangeViewModel oluşturuldu

---
+++ WebApplication1/ViewModels/PasswordChangeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentity.Web.ViewModels
+{
+    public class PasswordChangeViewModel
+    {
+        [Required(ErrorMessage = "Şifre alanı boş bırakılamaz")]
+        [Display(Name = "Şifre")]
+        public string OldPassword { get; set; }
+
+        [Required(ErrorMessage = "Yeni Şifre alanı boş bırakılamaz")]
+        [Display(Name = "Yeni Şifre")]
+        public string NewPassword { get; set; }
+
+        [Compare(nameof(NewPassword), ErrorMessage = "Şifreler eşleşmiyor")]
+        [Required(ErrorMessage = "Yeni Şifre Tekrar alanı boş bırakılamaz")]
+        [Display(Name = "Yeni Şifre Tekrar")]
+        public string NewPasswordConfirm { get; set; }
+    }
+}
