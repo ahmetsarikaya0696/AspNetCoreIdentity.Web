79f41c578603c48cd9a379193f64703ec7c91f78
ahmetsarikaya0696 <ahmetsarikaya0696@gmail.com>
2024-03-28 23:56:13 +0300

    AppUser entitysine ek propertylerin eklenmesi

--- WebApplication1/Models/AppUser.cs
+++ WebApplication1/Models/AppUser.cs
@@ -5,5 +5,8 @@ namespace AspNetCoreIdentity.Web.Models
     public class AppUser : IdentityUser
     {
         public string City { get; set; }
+        public string Picture { get; set; }
+        public DateTime? Birthday { get; set; }
+        public int? Gender { get; set; }
     }
 }
