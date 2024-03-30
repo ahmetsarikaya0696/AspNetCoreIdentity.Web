d1493f276fa07b59f7ba6d919a99eabebde49ea4
ahmetsarikaya0696 <ahmetsarikaya0696@gmail.com>
2024-03-30 15:15:16 +0300

    Rol oluşturmayla ilgili işlemler yapıldı

---
+++ WebApplication1/Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,50 @@
+﻿using AspNetCoreIdentity.Web.Areas.Admin.Models;
+using AspNetCoreIdentity.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIdentity.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RolesController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RolesController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRole(AddRoleViewModel addRoleViewModel)
+        {
+            var result = await _roleManager.CreateAsync(new AppRole() { Name = addRoleViewModel.Name });
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+
+                return View(result);
+            }
+
+
+            TempData["SuccessMessage"] = "Rol başarıyla eklendi";
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ WebApplication1/Areas/Admin/Models/AddRoleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentity.Web.Areas.Admin.Models
+{
+    public class AddRoleViewModel
+    {
+        [Required(ErrorMessage = "Ad alanı zorunludur!")]
+        [Display(Name = "Ad")]
+        public string Name { get; set; }
+    }
+}
