1d18aeb4e6a82302d95bf73100e19f276fa8411d
ahmetsarikaya0696 <ahmetsarikaya0696@gmail.com>
2024-03-31 12:50:39 +0300

    Atanan rollerin üye listesinde gösterilmesi işlemi uygulandı

---
+++ WebApplication1/TagHelpers/UserRoleNamesTagHelper.cs
@@ -0,0 +1,34 @@
+﻿using AspNetCoreIdentity.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text;
+
+namespace AspNetCoreIdentity.Web.TagHelpers
+{
+    public class UserRoleNamesTagHelper : TagHelper
+    {
+        public string UserId { get; set; }
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserRoleNamesTagHelper(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var user = await _userManager.FindByIdAsync(UserId);
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            var stringBuilder = new StringBuilder();
+
+            roles.ToList().ForEach(role =>
+            {
+                stringBuilder.Append(@$"<span class='badge badge-primary text-dark me-1'>{role.ToLower()}</span>");
+            });
+
+            output.Content.SetHtmlContent(stringBuilder.ToString());
+        }
+    }
+}
